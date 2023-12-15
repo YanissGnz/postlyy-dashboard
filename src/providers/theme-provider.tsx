c1e45dc99d3944849825c78c71d331123ba6158d
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2023-12-15 18:28:16 +0100

    add progress bar

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,9 +1,28 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+import { Next13ProgressBar } from "next13-progressbar";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      {/* <NextProgress
+        color="#1DA1F2"
+        height={3}
+        options={{
+          showSpinner: false,
+        }}
+      /> */}
+      {children}{" "}
+      <Next13ProgressBar
+        color="#1DA1F2"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="4px"
+        options={{ showSpinner: true }}
+        showOnShallow
+      />
+    </NextThemesProvider>
+  );
 }
