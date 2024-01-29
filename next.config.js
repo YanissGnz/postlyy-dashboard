00fa3479f783933a1fe0f71e066392710fe521ae
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2024-01-29 14:05:15 +0100

    fix font

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/types/env.js");
+await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 
