abf5bfbe6965b93809ce51cb664b931c384499ff
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2024-02-20 16:21:10 +0100

    change coming soon

--- src/app/coming-soon.tsx
+++ src/app/coming-soon.tsx
@@ -15,8 +15,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import Iconify from "@/components/ui/icon";
 import { toast } from "sonner";
+import BackgroundBeams from "./BackgroundBeams";
 
-export default function ComingSoon() {
+function ComingSoon() {
   const [subscribe, { isLoading, isSuccess }] = useSubscribeMutation();
 
   const formSchema = z.object({
@@ -42,57 +43,68 @@ export default function ComingSoon() {
   };
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
-      <h1 className="text-5xl font-semibold">Coming Soon!</h1>
-      <p className=" text-center text-muted-foreground">
-        Postlyy is currently in development. <br /> We are working hard to bring
-        you the best experience possible.
-      </p>
-      <p className=" text-center text-muted-foreground">
-        Subscribe to our newsletter to get notified when we launch and much
-        more.
-      </p>
-      {!isSuccess ? (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="flex ">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      className="rounded-e-none bg-background"
-                      placeholder="Enter your email"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              type="submit"
-              className="rounded-s-none"
-              disabled={isLoading}
+    <div className="relative flex h-screen flex-col items-center justify-center bg-black antialiased">
+      <div className="mx-auto max-w-2xl p-4">
+        <h1 className="relative z-10 bg-gradient-to-b from-primary/50  to-primary bg-clip-text text-center font-sans text-lg  font-bold text-transparent md:text-7xl">
+          Join the waitlist
+        </h1>
+        <p></p>
+        <p className="relative z-10 mx-auto my-2 max-w-lg text-center text-sm text-neutral-500">
+          Postlyy is currently in development. <br /> We are working hard to
+          bring you the best experience possible.
+          <br />
+          Subscribe to our newsletter to get notified when we launch and much
+          more.
+        </p>
+        {!isSuccess ? (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="z-10 mt-4 flex"
             >
-              Subscribe
-            </Button>
-          </form>{" "}
-        </Form>
-      ) : (
-        <div className="flex flex-col items-center justify-center gap-2">
-          <Iconify
-            icon="material-symbols:check-circle-rounded"
-            width={60}
-            className="text-green-500"
-          />
-          <p className="mb-10 max-w-xl text-center text-xl text-white">
-            Thanks for subscribing! We're excited to share news, updates, and
-            exclusive offers with you.
-          </p>
-        </div>
-      )}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="z-10 w-full">
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        placeholder="Enter your email"
+                        className=" rounded-e-none border border-neutral-800 bg-neutral-950 p-2 text-white placeholder:text-neutral-700 focus:ring-2 focus:ring-teal-500"
+                      />
+                    </FormControl>
+                    <FormMessage {...field} />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="z-10 rounded-s-none"
+                disabled={isLoading}
+              >
+                Subscribe
+              </Button>
+            </form>{" "}
+          </Form>
+        ) : (
+          <div className="z-10 flex flex-col items-center justify-center gap-2">
+            <Iconify
+              icon="material-symbols:check-circle-rounded"
+              width={60}
+              className="text-green-500"
+            />
+            <p className="mb-10 max-w-xl text-center text-xl text-white">
+              Thanks for subscribing! We're excited to share news, updates, and
+              exclusive offers with you.
+            </p>
+          </div>
+        )}
+      </div>
+      <BackgroundBeams />
     </div>
   );
 }
+
+export default ComingSoon;
