640988c11d4edf5ad368d4a9fde70bcee4f839c0
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2024-03-07 17:26:11 +0100

    fix

--- src/guard/RoleBasedGuard.tsx
+++ src/guard/RoleBasedGuard.tsx
@@ -1,22 +1,24 @@
 "use client";
 
-import { type ReactNode } from "react";
-import forbidden from "public/images/403.png";
 import Image from "@/components/ui/image";
-import { type EUserType } from "@/types/EUserType";
+import { ETiers } from "@/types/ETiers";
+import { EUserType } from "@/types/EUserType";
 import { useSession } from "next-auth/react";
-import { type ETiers } from "@/types/ETiers";
+import forbidden from "public/images/403.png";
+import { type ReactNode } from "react";
 // ----------------------------------------------------------------------
 
 interface RoleBasedGuardProp {
-  accessibleRoles: EUserType[];
+  accessibleRoles?: EUserType[];
   accessibleTiers?: ETiers[];
+  needAccount?: boolean;
   children: ReactNode;
 }
 
 export default function RoleBasedGuard({
-  accessibleRoles,
-  accessibleTiers,
+  accessibleRoles = [EUserType.Manager, EUserType.Owner, EUserType.TeamMember],
+  accessibleTiers = [ETiers.Basic, ETiers.Pro, ETiers.Expert],
+  needAccount,
   children,
 }: RoleBasedGuardProp) {
   const session = useSession();
@@ -34,9 +36,22 @@ export default function RoleBasedGuard({
     );
   }
 
+  if (needAccount && !Boolean(session?.data?.user?.accounts)) {
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center gap-4 p-10">
+        <h1 className="text-4xl font-bold">Permission Denied</h1>
+        <p className="text-lg">
+          {" "}
+          You do not have permission to access this page
+        </p>
+        <Image src={forbidden.src} height={400} width={400} />
+      </div>
+    );
+  }
+
   if (
-    session?.data?.user?.userType &&
-    (!accessibleRoles.includes(session?.data?.user.userType) ||
+    Boolean(session?.data?.user) &&
+    (!accessibleRoles?.includes(session?.data?.user.userType) ||
       !accessibleTiers?.includes(session?.data?.user.tier))
   ) {
     return (
