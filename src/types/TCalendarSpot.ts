703e563d4c90c00b620cb45c9a373f1521423ebe
Guendouzi Mohamed Yaniss <guendouzi.mohamedyaniss@namla.cloud>
2024-03-13 16:39:21 +0100

    minor

--- src/types/TCalendarSpot.ts
+++ src/types/TCalendarSpot.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { EPostSpotType } from './EPostSpotType';
+import { EPostSpotType } from "./EPostSpotType";
 
 export const calendarSpotSchema = z
   .object({
@@ -26,20 +26,32 @@ export const calendarSpotSchema = z
       path: ["startTime"],
     },
   )
-  .refine((data) => !(data.type === EPostSpotType.Recurring && data.daysOfWeek?.length === 0), {
-    message: "Days of week are required for recurring posts.",
-    path: ["daysOfWeek"],
-  }).refine((data) => {
-    return data.type !== EPostSpotType.Recurring && data.start !== "";
-  }, {
-    message: "Date is required.",
-    path: ["start"],
-  }).refine((data) => {
-    return data.forLinkedIn || data.forTwitter;
-  }, {
-    message: 'At least one social media platform must be selected.',
-    path: ["forTwitter", "forLinkedIn"],
-  });
+  .refine(
+    (data) =>
+      !(data.type === EPostSpotType.Recurring && data.daysOfWeek?.length === 0),
+    {
+      message: "Days of week are required for recurring posts.",
+      path: ["daysOfWeek"],
+    },
+  )
+  .refine(
+    (data) => {
+      return data.type !== EPostSpotType.Recurring && data.start !== "";
+    },
+    {
+      message: "Date is required.",
+      path: ["start"],
+    },
+  )
+  .refine(
+    (data) => {
+      return Boolean(data.forLinkedIn) || Boolean(data.forTwitter);
+    },
+    {
+      message: "At least one social media platform must be selected.",
+      path: ["forTwitter", "forLinkedIn"],
+    },
+  );
 
 export type TCalendarSpot = z.infer<typeof calendarSpotSchema>;
 
