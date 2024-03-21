7deb208bd3b04bb1da63da65def0dcc9f6771223
Guendouzi Mohamed Yaniss <guendouzi.mohamedyaniss@namla.cloud>
2024-03-21 15:57:36 +0100

    minor

--- src/app/(dashboard)/post/notes-sheet.tsx
+++ src/app/(dashboard)/post/notes-sheet.tsx
@@ -1,23 +1,23 @@
-import React, { useCallback, type Dispatch, type SetStateAction } from "react";
+import { Spinner } from "@/components/ui/Spinner";
+import { Button } from "@/components/ui/button";
+import Iconify from "@/components/ui/icon";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { usePagination } from "../../../hooks/usePagination";
-import { Spinner } from "@/components/ui/Spinner";
-import ErrorCard from "../../../components/error-card";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import Iconify from "@/components/ui/icon";
-import { Button } from "@/components/ui/button";
 import { useGetNotesQuery } from "@/redux/api/notes/apiSlice";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { useCallback, type Dispatch, type SetStateAction } from "react";
+import ErrorCard from "../../../components/error-card";
+import { usePagination } from "../../../hooks/usePagination";
 
 type Props = {
   isOpen: boolean;
@@ -52,7 +52,10 @@ export default function NotesSheet({ isOpen, setIsOpen, openNote }: Props) {
           <SheetHeader>
             <SheetTitle>Notes</SheetTitle>
           </SheetHeader>
-          <ScrollArea className="" style={{ height: "calc(100vh - 64px)" }}>
+          <ScrollArea
+            className="space-y-2"
+            style={{ height: "calc(100vh - 64px)" }}
+          >
             {isLoading ? (
               <div className="flex h-56 items-center justify-center">
                 <Spinner />
@@ -60,7 +63,7 @@ export default function NotesSheet({ isOpen, setIsOpen, openNote }: Props) {
             ) : isSuccess ? (
               notes.data.length > 0 ? (
                 <div className="flex flex-col">
-                  <div className="flex flex-col">
+                  <div className="flex flex-col gap-2">
                     {notes.data.map((note) => (
                       <div className="flex items-center justify-between rounded border p-1">
                         <p className="font-medium">{note.name}</p>
