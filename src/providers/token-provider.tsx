7d6dc3ecdb0ed9291932af939e4ddc6f6cc242a8
Guendouzi Mohamed Yaniss <guendouzi.mohamedyaniss@namla.cloud>
2024-03-22 15:28:58 +0100

    fix

--- src/providers/token-provider.tsx
+++ src/providers/token-provider.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { Spinner } from "@/components/ui/Spinner";
 import { setAccount, setToken } from "@/redux/slices/authSlice";
 import { ROUTES } from "@/routes";
 import { useSession } from "next-auth/react";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
@@ -27,21 +26,14 @@ export default function TokenProvider({
       ) {
         dispatch(setAccount(session.data.user.accounts[0]));
       }
-    } else if (session.status === "unauthenticated") {
+    } else if (
+      session.status === "unauthenticated" ||
+      (!session?.data?.user?.accessToken && session.status === "loading")
+    ) {
       localStorage.removeItem("token");
       push(ROUTES.login);
     }
   }, [session]);
-
-  if (session.status === "loading")
-    return (
-      <div className="flex h-screen w-screen items-center justify-center">
-        <Spinner />
-      </div>
-    );
-
-  if (session.status === "unauthenticated" || !session.data?.user?.accessToken)
-    redirect(ROUTES.login);
 
   return <>{children}</>;
 }
