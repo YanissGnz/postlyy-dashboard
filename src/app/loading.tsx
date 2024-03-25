31631c99511f62248ff307417ba80dc0b0fbfb02
Guendouzi Mohamed Yaniss <guendouzi.mohamedyaniss@namla.cloud>
2024-03-25 14:19:18 +0100

    minor

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 
 export default function loading() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center ">
-      <Image src="/icons/logo.png" alt="logo" width={100} height={100} />
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <Image
+        src="/icons/logo-transparent.png"
+        alt="logo"
+        width={100}
+        height={100}
+      />
       <Spinner />
     </div>
   );
